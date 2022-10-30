9632e4e6d0228ed9f0c9263e948213f49116e4da
Gunjan <85599079+Gunjan56@users.noreply.github.com>
2022-10-30 11:50:12 +0530

    Add files via upload

---
+++ basic/second.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+//int y =10; 
+using namespace std;
+
+int main(){
+// cin for user input
+    //  int x;
+    //  cout<<"enter the value you want to store in x \n";
+    //  cin>>x;
+
+    // cout<<x;
+    // declare and intialization
+    // int x;
+    // x = 100;
+
+    // int x = 100;
+    // int x; //declare x
+    // int y; //declare y
+
+    // x=100;
+    // y=100; //intialization
+
+    // int z = 400; //declare and intialization
+    // int x;
+    // cin>>x; //dynamic intialization
+
+    // int a; //size of data types
+    // char b;
+    // float c;
+    // long int d;
+    // long long int e;
+    // double f;
+
+    // int size = sizeof(a);
+    // cout<<size <<" bytes" <<endl;
+
+    // int size1 = sizeof(b);
+    // cout<<size1 <<" bytes" <<endl;
+
+    // int size2 = sizeof(c);
+    // cout<<size2 <<" bytes" <<endl;
+
+    // int size3 = sizeof(d);
+    // cout<<size3 <<" bytes" <<endl;
+
+    // int size4 = sizeof(e);
+    // cout<<size4 <<" bytes" <<endl;
+
+    // int size5 = sizeof(f);
+    // cout<<size5 <<" bytes";
+
+    // short x=20; //implicit type conversion
+    // int y=x;
+    // cout<<x<<endl;
+    // cout<<y;
+
+    // double x=1.2; //explicit type conversion
+    // int y = (int)x;
+    // cout<<y;
+
+    // int x; //local variable
+    // cout<<y;  //global variable
+    
+// number system
+    // int amount1;
+    // int amount2;
+    
+    // cin>>amount1;
+    // cout<<amount1<<endl;
+    // cin>>amount2;
+    // cout<<amount2<<endl;
+    // int sum = amount1 + amount2;
+    // cout<<"sum " <<sum;
+
+    //Bitwise and(&) operator
+    // int a=12, b=25;
+    // cout<<" a = "<<a <<endl;
+    // cout<<" b = "<<b <<endl;
+    // cout<<"a&b = "<<(a & b)<<endl;
+
+    //bitwise or(|) operator
+    // int a=12, b=25;
+    // cout<<" a = "<<a <<endl;
+    // cout<<" b = "<<b <<endl;
+    // cout<<"a|b = "<<(a | b)<<endl;
+
+    //bitwise xor(^) operator
+    // int a=12, b=25;
+    // cout<<" a = "<<a <<endl;
+    // cout<<" b = "<<b <<endl;
+    // cout<<"a^b = "<<(a ^ b)<<endl;
+    
+    //bitwise complement(~) operator
+    int num1=35, num2=-150;
+    cout<< "~(" << num1 << ") = " << (~num1) <<endl;
+    cout<< "~(" << num2 << ") = " << (~num2) <<endl;
+
+
+    return 0;
+} 
