31fd3af0968482cc608f9fd1b78c7bf56cf23ebb
Gunjan <85599079+Gunjan56@users.noreply.github.com>
2022-11-06 12:16:50 +0530

    Add files via upload

---
+++ basic/fourth.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+// While loop
+    // while (i<=10)
+    // {
+    //     cout<<"Hello dsa"<<endl;
+    //     i=i+1;
+    // }
+    
+    // Infinite loop
+    // while (i<=10)
+    // {
+    //     cout<<"Hello dsa"<<endl;
+    //     i=i+1;
+    // }
+    
+
+    //For loop
+    // for (int i = 10; i<=10; i=i+1)
+
+    // {
+    //     cout<<"Hello dsa"<<endl;
+    // }
+    
+    //do while loop
+    do
+    {
+        cout<<"hello dsa"<<endl;
+        i = i+1;
+    } while (i<=0);
+
+    return 0;
+}
