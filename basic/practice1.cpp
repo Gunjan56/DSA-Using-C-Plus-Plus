501c84287f7cf7327a6b15d3f6f7590e25a79e73
Gunjan <85599079+Gunjan56@users.noreply.github.com>
2022-11-20 19:35:38 +0530

    Add files via upload

---
+++ basic/practice1.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //5.1 print a hello world programme
+    //cout<<"Hello Basic Programe Part 1";//First Question
+
+    //5.2 Add two numbers given by user
+    // int a, b;
+    // cout<<"Enter First number = ";
+    // cin>>a;
+
+    // cout<<"Enter Second number = ";
+    // cin>>b;
+    // cout<<"Sum of two number = "<<(a+b);
+
+    //5.3 Write a program to print add substract division and remainder of two number..cout Approach
+    // int a,b;
+    // cout<<"Enter first number = ";
+    // cin>>a;
+    // cout<<"Enter second number = ";
+    // cin>>b;
+
+    // cout<<"Addition of two number = "<<(a+b)<<endl;
+    // cout<<"Substraction of two number = "<<(a-b)<<endl;
+    // cout<<"Multiplication of two number = "<<(a*b)<<endl;
+    // cout<<"devision of two number = "<<(a/b)<<endl;
+    // cout<<"Remainder of two number = "<<(a%b)<<endl;
+
+    //5.4 Write a programe to print next number and previous from int passed
+    // int n;
+    // cout<<"Enter the value of n = ";
+    // cin>>n;
+    // if(n>0){
+    // cout<<"Next integer value is = "<<(n+1) <<" "<<(n-1);
+    // }
+    // else if(n==0){
+    //    cout<<"Next integer value is = "<<(n+1) <<" "<<(n-1); 
+    // }
+    // else{
+    //     cout<<"Next integer value is = "<<(n-1) <<" "<<(n+2); 
+    // }
+
+    //5.5 write a program to check if two number are equal
+    // int a,b;
+    // cout<<"Enter first number = ";
+    // cin>>a;
+
+    // cout<<"Enter second number = ";
+    // cin>>b;
+    // if(a==b){
+    //     cout<<"Yes, Two numbers are equal";
+    // }else{
+    //     cout<<"No, Two numbers are not equal";
+    // }
+
+    //5.6 Write a program to check if two alphabets are equal
+    // char a,b;
+    // cout<<"Enter first Alphabet = ";
+    // cin>>a;
+
+    // cout<<"Enter Second Alphabet = ";
+    // cin>>b;
+
+    // if(a==b){
+    //     cout<<"Yes, Both alphabets are equal";
+    // }else{
+    //     cout<<"No, Both alphabets are not equal";
+    // }
+
+    //5.7 Write a program to check if entered alphabets are vowels or not
+    // char n;
+    // cout<<"Enter the alphabet = ";
+    // cin>>n;
+
+    // if(n=='a' || n=='e' || n=='i' || n=='o' || n=='u' || n=='A' || n=='E' || n=='I' || n=='O' || n=='U'){
+    //     cout<<"Yes, the entered alphabet is vowel";
+    // }else{
+    //     cout<<"No, the entered alphabet is not vowel";
+    // }
+
+    //5.8 Write a program to check if two entered text is same or not
+    // string a, b;
+    // cout<<"Enter First text = ";
+    // cin>>a;
+
+    // cout<<"Enter Second text = ";
+    // cin>>b;
+    // if(a==b){
+    //     cout<<"Yes, Both text are same";
+    // }else{
+    //     cout<<"No, Both are not same";
+    // }
+
+
+    return 0;
+}
