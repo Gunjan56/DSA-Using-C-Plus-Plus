f94611835bd7260770f9d7edd6df72605e2bc8df
Gunjan <85599079+Gunjan56@users.noreply.github.com>
2022-12-19 17:34:51 +0530

    Add files via upload

---
+++ basic/practice2.cpp
@@ -0,0 +1,195 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //5.9 print area of triangle
+    // int a,b;
+    //float area;
+    // cout<<"Enter base of triangle: ";
+    // cin>>a;
+    // cout<<"Enter height of triangle: ";
+    // cin>>b;
+    //area = (a*b)/2;
+    // cout<<"Area of triangle: "<<area;
+    // cout<<"Area of triangle: "<<(a*b)/2;
+
+    //5.10 print area of rectangle
+    // int a,b;
+    // int area;
+
+    // cout<<"Enter lenght of rectangle: ";
+    // cin>>a;
+
+    // cout<<"Enter width of rectangle: ";
+    // cin>>b;
+
+    // area = a*b;
+    // //cout<<"Area of rectangle: "<<area;
+    // cout<<"Area of rectangle: "<<(a*b);
+
+    //5.11 print area of circle
+    // int r;
+    // int area;
+
+    // cout<<"Enter the radius of circle: ";
+    // cin>>r;
+    // area=(3.14*r*r);
+    // //cout<<"Area of cirle: "<<area;
+    // cout<<"Area of cirle: "<<(3.14*r*r);
+
+    //5.12 print the area of ellipse
+    // int a;
+    // int b;
+    // float c;
+    // cout<<"Enter a axis = ";
+    // cin>>a;
+
+    // cout<<"Enter b axis = ";
+    // cin>>b;
+    // c=(3.14*a*b);
+    // // cout<<"Area of ellipse : "<<(3.14*a*b);
+    // cout<<"Area of ellipse : "<<c;
+    
+    // calculate the simple interest
+    // int p;
+    // int r;
+    // float t;
+    // float si;
+    // cout<<"Enter principle: Rs ";
+    // cin>>p;
+
+    // cout<<"Enter rate: % ";
+    // cin>>r;
+
+    // cout<<"Enter time: years ";
+    // cin>>t;
+
+    // si = (p*r*t)/100; 
+    // //cout<<"Simple interes: "<<(p*r*t)/100;
+    // cout<<"Simple interes: "<<si;
+
+    //5.13 write a c++ program to convert temprature(c.f)
+
+    // int c;
+    // float res;
+    // cout<<"Enter temprature c: ";
+    // cin>>c;
+    // res = ((1.8*c)+32);
+    // cout<<"temprature in f: "<<((1.8*c)+32);
+    // cout<<"temprature in f: "<<res;
+
+    //5.14 write a program to take input from user as 5 subject(max marks per subject is 100)to calculate percentage
+    // int s1, s2, s3, s4, s5;
+    // int per;
+    // cout<<" Enter marks in subject: ";
+    // cin>>s1>>s2>>s3>>s4>>s5;
+
+    // per = (s1+s2+s3+s4+s5)*100/500;
+    // // cout<<"Percentage: "<<(s1+s2+s3+s4+s5)*100/500<<"%";
+    // cout<<"Percentage: "<<per<<"%";
+
+    //5.15 write a program to find n% of t
+    // int n, t;
+    // double res;
+    // cout<<"n% of t\n";
+    // cout<<"n% of : ";
+    // cin>>n;
+
+    // cout<<"t: ";
+    // cin>>t;
+
+    // res = (n*t)/100;
+    // cout<<"Result = "<<(n*t)/100;
+    // cout<<"Result = "<<res;
+
+    //conversion based questions
+    //write a programe to convert minutes into seconds
+    // int n;
+    // cout<<"Enter time in minute: ";
+    // cin>>n;
+
+    // cout<<"time in seconds: "<<(n*60)<<" seconds";
+
+    //5.18 write a programe to convert hours into minutes
+    // int h;
+    // cout<<"Enter time in hours: ";
+    // cin>>h;
+
+    // cout<<"Time in minutes: "<<(h*60)<<" min";
+
+    //5.19 write a program to convert hours into second
+    // int h;
+    // cout<<"Enter time in hours: ";
+    // cin>>h;
+
+    // cout<<"Time in seconds: "<<(h*60*60)<<" seconds";
+
+    //5.20 write a program to convert kilometer into meter
+    // int k;
+    // cout<<"Enter the distance in km: ";
+    // cin>>k;
+
+    // cout<<"distance in meter: "<<(k*1000)<<" meter";
+
+    //5.21 write a program to convert meter into kilometer
+    //int m;
+    // float m;
+    // cout<<"Enter the distance in meter: ";
+    // cin>>m;
+
+    // cout<<"distance in km: "<<(m/1000)<<" Km";
+
+    //5.22 write a program to convert mile into kilometer
+    // int m;
+    // cout<<"Enter distance in miles: ";
+    // cin>>m;
+
+    // cout<<"distance in km: "<<(m*1.6)<<" km";
+
+    //5.23 write a program to convert kilogram into gram
+    // int k;
+    // cout<<"Enter the value in kg: ";
+    // cin>>k;
+    // cout<<"Value in Grams: "<<(k*1000)<<" gram";
+
+    //5.24 write a program to square of the given number
+    // int n;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // cout<<"Square of the given number: "<<(n*n);
+
+    //Theory based questions
+    //5.25 write a program to count the total amount that farmer have to spend
+    // int c;
+    // int g;
+    // int d;
+    // int total_legs;
+    // int total_amount;
+    // cout<<"Enter total no of chicken: ";
+    // cin>>c;
+
+    // cout<<"Enter total no of goats: ";
+    // cin>>g;
+
+    // cout<<"Enter toatal no of dogs: ";
+    // cin>>d;
+
+    // total_legs = (2*c)+(4*g)+(4*d);
+    // cout<<"Total number of legs: "<<total_legs<<endl;
+
+    // total_amount = (total_legs*300);
+    // cout<<"Total amount = Rs "<<total_amount;
+
+    //5.26 write a program for above problem
+    // int a;
+    // cout<<"Enter the amount: ";
+    // cin>>a;
+
+    // if(a<=5000){
+    //     cout<<"No GST will be applied for your item.";
+    // }else{
+    //     cout<<"Gst will be added with your item."<<endl<<"Final cost you have to pay is: "<<(a+500)<<" Rs";
+    // }
+    
+    return 0;
+}
